023c7e9b875528d3d6ff45eefd1a60ead8349281
nidhi03github <69838021+nidhi03github@users.noreply.github.com>
2020-09-09 17:20:37 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,45 @@
+import config
+import sys
+import traceback
+import time
+
+from door_detect import Detector
+from door_detect.cameras import Foscam
+
+
+if __name__ == '__main__':
+  try:
+    # used for local testing
+    #inputImage = cv2.imread('door_down.jpg')
+    previousState = None
+
+    while True:
+        # set up camera object
+        camera = Foscam(config.CAMADDRESS, config.USERNAME, config.PASSWORD)
+
+        # get live webcam data in OpenCV format
+        inputImage = camera.getSnapshotCV()
+
+        # set up our detector object
+        detector = Detector(config.THRESHOLD, config.SHAPES, config.TEMPLATEDIR, config.HORIZ_ALIGN_THRESH, config.OUTPUTDIR, config.OUTPUT_DETECTION_IMAGE)
+
+        # get the current state of the door
+        currentState = detector.detectState(inputImage)
+        
+        # experimental
+        #if(previousState != None):
+        #    statesDiff = detector.diffStates(currentState, previousState)
+        #    print statesDiff
+
+        # output the state of the door
+        print "Current state: ", currentState[0]
+
+        #previousState = currentState
+        time.sleep(.2)
+
+
+  except Exception, e:
+    print "Fatal error."
+    print e
+    print traceback.format_exc()
+    sys.exit(1)
